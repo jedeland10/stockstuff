00caf01b4e5c49dac14232e37197babf79c593eb
jedeland10 <johan.edeland@live.com>
2026-03-20 11:39:54 +0100

    local dev setup

--- app/config.py
+++ app/config.py
@@ -2,10 +2,10 @@ import os
 
 # Nordic exchanges
 EXCHANGES = {
-    "SE": ".ST",    # Stockholm
-    "FI": ".HE",    # Helsinki
-    "DK": ".CO",    # Copenhagen
-    "NO": ".OL",    # Oslo
+    "SE": ".ST",  # Stockholm
+    "FI": ".HE",  # Helsinki
+    "DK": ".CO",  # Copenhagen
+    "NO": ".OL",  # Oslo
 }
 
 UPDATE_INTERVAL_HOURS = int(os.getenv("UPDATE_INTERVAL_HOURS", "6"))
